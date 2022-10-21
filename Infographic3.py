c2efdfcc04ca83afbd1a587181d52b7add9318c1
Shac <116039413+Shac2799@users.noreply.github.com>
2022-10-21 16:41:25 +0100

    Update Infographic3.py

--- Infographic3.py
+++ Infographic3.py
@@ -4,6 +4,8 @@ import math
 import pandas as pd
 import streamlit as st
 
+st.title("Infographic Showing Effect of Mass on Gravitational Strength")
+
 Body_size = st.slider("Diameter of Celestial Body [km]", 1, 10000, 5000)
 
 
