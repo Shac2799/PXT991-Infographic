7ee12a250575e27fe3224005f1d5271dec1b0049
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-10 18:49:29 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -67,13 +67,13 @@ def main():
     mass_body = st.slider("Mass of body [Solar mass]", min_value = 1.0, max_value = 10.0, step = 0.5, value = 1.0)
     init_vel = st.slider("Object initial velocity [km/s]", min_value = -30.0, max_value = 30.0, step = 5.0, value = 0.0)
     #Angle = st.slider("Angle of trajectory [Degrees]", min_value = -90.0, max_value = 90.0, step = 1.0)
-    Days = st.slider("Duration [Days]", min_value = 0.0, max_value = 1000.0, step = 5.0,value = 0.0)
+    Days = st.slider("Duration [Days]", min_value = 0.0, max_value = 5000.0, step = 5.0,value = 0.0)
     init_x1 = st.slider("Initial x coordinate of object 1 (red)", min_value = -5.0, max_value = 5.0, step = 0.5,value = 1.0)
     init_y1 = st.slider("Initial y coordinate of object 1 (red)", min_value = -5.0, max_value = 5.0, step = 0.5,value = 0.0) 
     Earth = Object(init_x1,init_y1,init_vel,90,5.97e24,Days) 
     sun = Body(696.343e6,mass_body,0,0)
-    init_x2 = st.slider("Initial x coordinate of object 2 (blue)", min_value = -5.0, max_value = 5.0, step = 0.5,value = 0.0)
-    init_y2 = st.slider("Initial y coordinate of object 2 (blue)", min_value = -5.0, max_value = 5.0, step = 0.5,value = 1.0) 
+    init_x2 = st.slider("Initial x coordinate of object 2 (blue)", min_value = -5.0, max_value = 5.0, step = 0.5,value = 2.0)
+    init_y2 = st.slider("Initial y coordinate of object 2 (blue)", min_value = -5.0, max_value = 5.0, step = 0.5,value = 0.0) 
     comet = Object(init_x2,init_y2,init_vel,90,2.2e14,Days)#check why angle affects starting pos  
     #sun_scale = radius_sun/Earth.AU  #  to be more accurate to scale of solar system
     for day in range(int(Days)):
