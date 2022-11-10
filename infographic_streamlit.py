f8dd234d02be4c9d5286588b21b0af8a31c50670
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-10 18:18:22 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -77,8 +77,8 @@ def main():
         comet.update_path(sun)
         fig = plt.figure(figsize=(10,8))
         plt.scatter(sun.centre_x,sun.centre_y, color = 'y' , s = 100)
-        plt.scatter(Earth.x,Earth.y, color = 'r', s = 3)
-        plt.scatter(comet.x,comet.y, color = 'b', s = 3)
+        plt.scatter(Earth.path_x,Earth.parth_y, color = 'r', s = 3)
+        plt.scatter(comet.path_x,comet.path_y, color = 'b', s = 3)
 main()
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
