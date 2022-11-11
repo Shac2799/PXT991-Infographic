b4d5a3ccc83d87a633e7c7a8092001c180c4bf86
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-11 00:04:53 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -83,7 +83,7 @@ def main():
     plt.scatter(sun.centre_x,sun.centre_y, color = 'y' , s = 500)
     plt.scatter(Earth.path_x,Earth.path_y, color = 'r', s = 10)
     plt.scatter(comet.path_x,comet.path_y, color = 'b', s = 10)
-    plt.set_facecolor('k')
+    plt.rcParams['axes.facecolor'] = 'black'
 main()
 
 st.pyplot(fig=None, clear_figure=None)
