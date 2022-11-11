2d7cbd372a38cbe829245874f6e13970e77832a6
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-11 11:27:22 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -87,7 +87,7 @@ def main():
     plt.xlabel("Distance [AU]")
     plt.ylabel("Distance [AU]")
     plt.rcParams['axes.facecolor'] = 'black'
-    plt.set_aspect(aspect='equal')
+    fig.set_aspect(aspect='equal')
 main()
 
 st.pyplot(fig=None, clear_figure=None)
