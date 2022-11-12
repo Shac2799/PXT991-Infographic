40c5a4207b52822c26f8db81be1a4d88bbcaa335
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-12 21:08:38 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -127,6 +127,7 @@ def main():
     plt.yticks(oldy,AU)
     #plt.rcParams['axes.facecolor'] = 'black'
     plt.show()
+    plt.plot(Earth.KE)
 #     energy_data = pd.DataFrame({'Ek':Earth.KE,'PE': Earth.PE})
 #     st.line_chart(energy_data,x = 'Days', y = 'Energy [joules]')
     #st.line_chart(Earth.L,x = 'Days', y = 'Dngular momentum')
