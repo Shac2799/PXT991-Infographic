34033c39f8eb5a1c8d2dea018928cd9bcabb6cb1
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-15 20:25:35 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -164,7 +164,7 @@ def main():
       ax.add_artist(ab_asteroidimg)
 
     #plotting earth
-    px.scatter(earth_x,earth_y, color = 'b', s = 0.3) # plot Earth
+    px.scatter(earth_x,earth_y, color = 'b', size = 0.3) # plot Earth
     imagebox_earth = OffsetImage(Earth_img, zoom = 0.02)
     ab_earthimg = AnnotationBbox(imagebox_earth, [earth_x[-1],earth_y[-1]], xycoords = 'data', frameon = False)
     px.add_artist(ab_earthimg)
