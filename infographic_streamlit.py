c24ea73176a6458c521b017a2b8ce8a898e4bd62
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-15 21:09:41 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -165,6 +165,12 @@ def main():
 
     #plotting earth
     # Plots the Earth png in most recent positio 
+    paths_df = pd.DataFrame({'x-coords':earth_x,'y-coords':earth_y,'day':Day})
+    fig2 = px.scatter(paths_df,x = 'x-coords',y = 'y-coords',
+               animation_frame='day',animation_group='x-coords')
+    fig2.update_layout()
+    st.write(fig2)
+    
     ax.scatter(earth_x,earth_y, color = 'b', s = 0.3) # plot Earth
     imagebox_earth = OffsetImage(Earth_img, zoom = 0.02)
     ab_earthimg = AnnotationBbox(imagebox_earth, [earth_x[-1],earth_y[-1]], xycoords = 'data', frameon = False)
