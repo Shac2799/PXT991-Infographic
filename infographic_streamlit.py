63b4818b16b9b585a10aa86b5ee11c3091c34fb2
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-15 15:43:40 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -150,12 +150,6 @@ def main():
     fig,ax = plt.subplots()
     plt.imshow(stars) # plot background image
     
-    #plotting sun 
-    ax.scatter(sun_scaledx,sun_scaledy, color = 'tab:orange' , s = 1) # plot sun positio
-    imagebox_sun = OffsetImage(Sun_img, zoom = 0.06)
-    ab_sunimg = AnnotationBbox(imagebox_sun, [sun_scaledx, sun_scaledy], xycoords = 'data', frameon = False)
-    ax.add_artist(ab_sunimg)
-    
     #plotting earth
     ax.scatter(earth_x,earth_y, color = 'b', s = 1) # plot Earth
     imagebox_earth = OffsetImage(Earth_img, zoom = 0.02)
@@ -168,6 +162,13 @@ def main():
     imagebox_asteroid = OffsetImage(Asteroid_img, zoom = 0.02)
     ab_asteroidimg = AnnotationBbox(imagebox_asteroid, [asteroid_x[-1],asteroid_y[-1]], xycoords = 'data', frameon = False)
     ax.add_artist(ab_asteroidimg)
+    
+        
+    #plotting sun 
+    ax.scatter(sun_scaledx,sun_scaledy, color = 'tab:orange' , s = 1) # plot sun positio
+    imagebox_sun = OffsetImage(Sun_img, zoom = 0.06)
+    ab_sunimg = AnnotationBbox(imagebox_sun, [sun_scaledx, sun_scaledy], xycoords = 'data', frameon = False)
+    ax.add_artist(ab_sunimg)
 
     # changing limits/labels for axes
     plt.xlim(xlim)
