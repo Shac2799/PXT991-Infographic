5567a93d33af5ebc312737bb8b50691671216b41
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-15 15:21:44 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -123,16 +123,18 @@ def main():
     
     #iterating through each day to update paths
     for day in range(int(Days)): # get complete array of positions of objects over x days 
-      Earth.danger_zone(sun)
       asteroid.danger_zone(sun)
       if asteroid.No_danger == True:
         asteroid.update_path(sun)
       else:
-        pass
+        break
+        
+     for day in range(int(Days)):
+      Earth.danger_zone(sun)
       if Earth.No_danger == True:
         Earth.update_path(sun)
       else:
-        pass
+        break
     
     #re-scaling to image dimensions
     earth_x,earth_y,xlim,ylim = Earth.rescale_grid(stars, x_lim, y_lim) 
