8a7052ea3f8397373a02f16dc4fbb737dc647dfb
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-18 13:50:26 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -133,7 +133,7 @@ class Object:
 def main():
     # choose steps/values for sliders
         
-    mass_ast = st.number_input("Select the mass of the asteroid (in Earth masses)", step = 0.5, value = 1.0, min_value = 0.1)
+    mass_ast = st.number_input("Select the mass of the asteroid (in Earth masses)", step = 0.5, value = 1.0, min_value = 1e-30)
     conv_mass = mass_ast*5.97e24
     st.write("The asteroid's mass is ",conv_mass, " kg")
     
