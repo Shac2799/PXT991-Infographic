73384bc65cd023512095c1aaa716caa71583e655
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-30 14:49:24 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -160,7 +160,7 @@ def main():
     asteroid_x,asteroid_y,_,_ = asteroid.rescale_grid(stars, x_lim, y_lim) 
     
     #defining parameters to change axes limits to AU
-    oldx = np.linspace(0,width,7)
+    oldx = np.linspace(0,height,7)
     oldy = np.linspace(0,height,7)
     AU = [-3,-2,-1,0,1,2,3]          
     
