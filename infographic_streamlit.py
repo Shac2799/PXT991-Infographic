9fa77ed2f29485c024063515e0d41c2be685967c
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-30 17:58:18 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -110,17 +110,26 @@ class Object:
         c1 = c4 = w1/2
         c2 = c3 = (w0+w1)/2
         
-        #intemediary steps between each time interval
+        #intemediary steps between each time interval for x velocity
         #coefficients sum to 1 to ensure that total dt = time_interval
         self.x += c1*self.x_vel*self.time_interval #x1
-        self.xvel += d1*(self.cowells(body)/self.mass)*self.time_interval #v1
+        self.xvel += d1*(self.cowells(body,objects)/self.mass)*self.time_interval #v1
         self.x += c2*self.x_vel*self.time_interval # x2
-        self.xvel += d2*(self.cowells(body)/self.mass)*self.time_interval #v2
+        self.xvel += d2*(self.cowells(body,objects)/self.mass)*self.time_interval #v2
         self.x += c3*self.x_vel*self.time_interval # x3      
-        self.xvel += d3*(self.cowells(body)/self.mass)*self.time_interval #v3
+        self.xvel += d3*(self.cowells(body,objects)/self.mass)*self.time_interval #v3
         self.x += c4*self.x_vel*self.time_interval # x4
         
         self.path_x.append(self.x/self.AU) # storing position in array
+        
+        self.y += c1*self.y_vel*self.time_interval #x1
+        self.yvel += d1*(self.cowells(body,objects)/self.mass)*self.time_interval #v1
+        self.y += c2*self.y_vel*self.time_interval # x2
+        self.yvel += d2*(self.cowells(body,objects)/self.mass)*self.time_interval #v2
+        self.y += c3*self.y_vel*self.time_interval # x3      
+        self.yvel += d3*(self.cowells(body,objects)/self.mass)*self.time_interval #v3
+        self.y += c4*self.y_vel*self.time_interval # x4
+        
         self.path_y.append(self.y/self.AU)
         # using euler method        
 #         self.x_vel += (net_fx/self.mass)*self.time_interval
