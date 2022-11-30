eef765c8e9a31eb2a04e312c2754979c6cea60f7
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-30 19:44:13 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -103,16 +103,16 @@ class Object:
 #           net_fx += fx
 #           net_fy += fy
         
-        self.i += 1
-        # adaptive timestep, halves time interval if difference between consecutive velocites > 1.25v or x1.25 if <1.25v
-        if self.i > 1:
-          ratiox, ratioy = (self.xvel_list[self.i]/self.xvel_list[self.i-1]), (self.yvel_list[self.i]/self.yvel_list[self.i-1])
-          if ratiox or ratioy > 1.5:
-              self.time_interval = self.time_interval/2
-          elif ratiox or ratioy <= 1: 
-              self.time_interval = 1.1*self.time_interval
+#         self.i += 1
+#         # adaptive timestep, halves time interval if difference between consecutive velocites > 1.25v or x1.25 if <1.25v
+#         if self.i > 1:
+#           ratiox, ratioy = (self.xvel_list[self.i]/self.xvel_list[self.i-1]), (self.yvel_list[self.i]/self.yvel_list[self.i-1])
+#           if ratiox or ratioy > 1.5:
+#               self.time_interval = self.time_interval/2
+#           elif ratiox or ratioy <= 1: 
+#               self.time_interval = 1.1*self.time_interval
                 
-        self.elapsed_time += self.time_interval
+#         self.elapsed_time += self.time_interval
    
         #using fourth order yoshida leapfrog integrator
         w0, w1 = -(np.cbrt(2)/(2-np.cbrt(2))), (1/(2-np.cbrt(2)))
