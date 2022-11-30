62ec3f36f648d41bf38fc1a1d0c97d6cd5316a17
Shac <116039413+Shac2799@users.noreply.github.com>
2022-11-30 18:18:10 +0000

    Update infographic_streamlit.py

--- infographic_streamlit.py
+++ infographic_streamlit.py
@@ -222,7 +222,7 @@ def main():
     plt.imshow(stars_cropped)
     #plotting asteroid
     if choice == "Add asteroid":
-      ax.plot(asteroid_x,asteroid_y, color = 'r',linewidth=1) # plot asteroid
+      ax.plot(asteroid_x,asteroid_y, color = 'r',linewidth=0.5) # plot asteroid
       imagebox_asteroid = OffsetImage(Asteroid_img, zoom = 0.02)
       ab_asteroidimg = AnnotationBbox(imagebox_asteroid, [asteroid_x[-1],asteroid_y[-1]], xycoords = 'data', frameon = False)
       ax.add_artist(ab_asteroidimg) # adding image of Earth to last coordinate in path
