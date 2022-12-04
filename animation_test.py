6314beac429ea5b2dd07bc94f68877552510446c
Shac <116039413+Shac2799@users.noreply.github.com>
2022-12-04 22:17:11 +0000

    Update animation_test.py

--- animation_test.py
+++ animation_test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import plotly.graph_objects as go
-
+import streamlit as st
 
 st.set_page_config(page_title="animation test", page_icon=None, 
                    layout="wide", initial_sidebar_state="auto", menu_items=None) 
